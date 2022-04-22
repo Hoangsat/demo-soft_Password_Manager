297bf1ce68333e5baee14cf7532cc83ab7208332
Hoangsat <hoangsat1@gmail.com>
2022-04-22 03:09:50 +0200

    Password manager

---
+++ password_manager/password_manager/FormPassword.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace password_manager
+{
+    partial class FormPassword
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPassword));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.bt_password3 = new System.Windows.Forms.Button();
+            this.tb_login3 = new System.Windows.Forms.TextBox();
+            this.llSite3 = new System.Windows.Forms.LinkLabel();
+            this.bt_password2 = new System.Windows.Forms.Button();
+            this.tb_login2 = new System.Windows.Forms.TextBox();
+            this.llSite2 = new System.Windows.Forms.LinkLabel();
+            this.llSite1 = new System.Windows.Forms.LinkLabel();
+            this.tb_login1 = new System.Windows.Forms.TextBox();
+            this.bt_password1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.bt_password3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tb_login3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.llSite3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bt_password2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tb_login2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.llSite2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.llSite1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tb_login1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.bt_password1, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(682, 193);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // bt_password3
+            // 
+            this.bt_password3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_password3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_password3.Location = new System.Drawing.Point(457, 131);
+            this.bt_password3.Name = "bt_password3";
+            this.bt_password3.Size = new System.Drawing.Size(222, 59);
+            this.bt_password3.TabIndex = 8;
+            this.bt_password3.Tag = "Toiyeuem3";
+            this.bt_password3.Text = "Password";
+            this.bt_password3.UseVisualStyleBackColor = true;
+            this.bt_password3.Click += new System.EventHandler(this.bt_password1_Click);
+            // 
+            // tb_login3
+            // 
+            this.tb_login3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_login3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_login3.Location = new System.Drawing.Point(230, 131);
+            this.tb_login3.Multiline = true;
+            this.tb_login3.Name = "tb_login3";
+            this.tb_login3.ReadOnly = true;
+            this.tb_login3.Size = new System.Drawing.Size(221, 59);
+            this.tb_login3.TabIndex = 7;
+            this.tb_login3.Text = "hoangsat3@gmail.com";
+            this.tb_login3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_login3.Click += new System.EventHandler(this.tb_login1_Click);
+            // 
+            // llSite3
+            // 
+            this.llSite3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.llSite3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.llSite3.Location = new System.Drawing.Point(3, 128);
+            this.llSite3.Name = "llSite3";
+            this.llSite3.Size = new System.Drawing.Size(221, 65);
+            this.llSite3.TabIndex = 6;
+            this.llSite3.TabStop = true;
+            this.llSite3.Tag = "vk.com";
+            this.llSite3.Text = "VK";
+            this.llSite3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.llSite3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llSite1_LinkClicked);
+            // 
+            // bt_password2
+            // 
+            this.bt_password2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_password2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_password2.Location = new System.Drawing.Point(457, 67);
+            this.bt_password2.Name = "bt_password2";
+            this.bt_password2.Size = new System.Drawing.Size(222, 58);
+            this.bt_password2.TabIndex = 5;
+            this.bt_password2.Tag = "Toiyeuem2";
+            this.bt_password2.Text = "Password";
+            this.bt_password2.UseVisualStyleBackColor = true;
+            this.bt_password2.Click += new System.EventHandler(this.bt_password1_Click);
+            // 
+            // tb_login2
+            // 
+            this.tb_login2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_login2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_login2.Location = new System.Drawing.Point(230, 67);
+            this.tb_login2.Multiline = true;
+            this.tb_login2.Name = "tb_login2";
+            this.tb_login2.ReadOnly = true;
+            this.tb_login2.Size = new System.Drawing.Size(221, 58);
+            this.tb_login2.TabIndex = 4;
+            this.tb_login2.Text = "hoangsat2@gmail.com";
+            this.tb_login2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_login2.Click += new System.EventHandler(this.tb_login1_Click);
+            // 
+            // llSite2
+            // 
+            this.llSite2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.llSite2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.llSite2.Location = new System.Drawing.Point(3, 64);
+            this.llSite2.Name = "llSite2";
+            this.llSite2.Size = new System.Drawing.Size(221, 64);
+            this.llSite2.TabIndex = 3;
+            this.llSite2.TabStop = true;
+            this.llSite2.Tag = "Videosharp.info";
+            this.llSite2.Text = "Videosharp";
+            this.llSite2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.llSite2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llSite1_LinkClicked);
+            // 
+            // llSite1
+            // 
+            this.llSite1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.llSite1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.llSite1.Location = new System.Drawing.Point(3, 0);
+            this.llSite1.Name = "llSite1";
+            this.llSite1.Size = new System.Drawing.Size(221, 64);
+            this.llSite1.TabIndex = 0;
+            this.llSite1.TabStop = true;
+            this.llSite1.Tag = "http://www.gmail.com";
+            this.llSite1.Text = "Gmail";
+            this.llSite1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.llSite1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llSite1_LinkClicked);
+            // 
+            // tb_login1
+            // 
+            this.tb_login1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tb_login1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_login1.Location = new System.Drawing.Point(230, 3);
+            this.tb_login1.Multiline = true;
+            this.tb_login1.Name = "tb_login1";
+            this.tb_login1.ReadOnly = true;
+            this.tb_login1.Size = new System.Drawing.Size(221, 58);
+            this.tb_login1.TabIndex = 1;
+            this.tb_login1.Text = "hoangsat1@gmail.com";
+            this.tb_login1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tb_login1.Click += new System.EventHandler(this.tb_login1_Click);
+            // 
+            // bt_password1
+            // 
+            this.bt_password1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bt_password1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bt_password1.Location = new System.Drawing.Point(457, 3);
+            this.bt_password1.Name = "bt_password1";
+            this.bt_password1.Size = new System.Drawing.Size(222, 58);
+            this.bt_password1.TabIndex = 2;
+            this.bt_password1.Tag = "Toiyeuem1";
+            this.bt_password1.Text = "Password";
+            this.bt_password1.UseVisualStyleBackColor = true;
+            this.bt_password1.Click += new System.EventHandler(this.bt_password1_Click);
+            // 
+            // FormPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 193);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Password Manager";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.LinkLabel llSite1;
+        private System.Windows.Forms.TextBox tb_login1;
+        private System.Windows.Forms.Button bt_password1;
+        private System.Windows.Forms.Button bt_password3;
+        private System.Windows.Forms.TextBox tb_login3;
+        private System.Windows.Forms.LinkLabel llSite3;
+        private System.Windows.Forms.Button bt_password2;
+        private System.Windows.Forms.TextBox tb_login2;
+        private System.Windows.Forms.LinkLabel llSite2;
+    }
+}
+
---
+++ password_manager/password_manager/FormPassword.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace password_manager
+{
+    public partial class FormPassword : Form
+    {
+        public FormPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void llSite1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start((string)((LinkLabel)sender).Tag);
+        }
+
+        private void tb_login1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText((string)((TextBox)sender).Text);
+           ((TextBox)sender).SelectAll();
+        }
+
+        private void bt_password1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText((string)((Button)sender).Tag);
+          
+
+        }
+    }
+}
